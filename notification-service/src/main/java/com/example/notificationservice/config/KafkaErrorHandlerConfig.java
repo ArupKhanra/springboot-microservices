1df2c7facadccd2f46aa920e4d7be7033f977a72
Arup Khanra <arupkhanra03091994@gmail.com>
2026-06-21 18:45:23 +0530

    Implemented Kafka Dead Letter Topic (DLT) handling

--- notification-service/src/main/java/com/example/notificationservice/config/KafkaErrorHandlerConfig.java
+++ notification-service/src/main/java/com/example/notificationservice/config/KafkaErrorHandlerConfig.java
@@ -1,20 +1,39 @@
 package com.example.notificationservice.config;
 
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaErrorHandlerConfig {
 
     @Bean
-    public DefaultErrorHandler errorHandler() {
+    public DefaultErrorHandler errorHandler(
+            KafkaTemplate<String, String> kafkaTemplate
+    ) {
 
         System.out.println("CUSTOM ERROR HANDLER LOADED");
 
+        DeadLetterPublishingRecoverer recoverer =
+                new DeadLetterPublishingRecoverer(
+                        kafkaTemplate,
+                        (record, exception) ->
+                                new TopicPartition(
+                                        "user-created-topic-dlt",
+                                        record.partition()
+                                )
+                );
+
         return new DefaultErrorHandler(
-                new FixedBackOff(5000L, 2L)
+                recoverer,
+                new FixedBackOff(
+                        5000L,
+                        2L
+                )
         );
     }
 }
